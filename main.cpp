f49951cc671c6f5ab5bc7ba517c0a585c6b69f07
jeremy <jeremy@jeremy-Inspiron-3793>
2021-08-26 11:10:46 -0500

    remove pitcher opponent data

--- main.cpp
+++ main.cpp
@@ -193,46 +193,6 @@ int main(int argc, char** argv) {
             }
     	}
 
-    	query = "select distinct gamedate from PBPHeader ph inner join PBPDetails pd on ph.id=pd.headerid where ph.gamedate < '"+datestr+"' and pd.pitcherid="+std::to_string(pitcherId)+" and "+(tmType == teamType::AWAY ? "ph.awayteam='"+opponent+"'" : "ph.hometeam='"+opponent+"'")+" and pd.inningtype='"+(tmType == teamType::AWAY ? "t" : "b")+"' and ph.gamenumber="+std::to_string(gameNumber)+" and ph.isNightGame="+(dayNight.compare("d") == 0 ? "0" : "1")+" and pd.isPitcherStarter=1 order by ph.gamedate desc";
-    	std::vector<std::map<std::string, std::string>> pOppDates = DBWrapper::queryDatabase(db, query);
-
-    	if (!pOppDates.empty()) {
-
-        	query = "select ph.gamedate,ph.isNightGame,pd.inningtype,pd.inningnum,pd.batpos,pd.hits,pd.event";
-        	query += " from PBPHeader ph inner join PBPDetails pd on ph.id=pd.headerid";
-        	query += " where ph.gamedate in (";
-        	std::string pOppDateStr = "";
-        	for (std::map<std::string, std::string> pOppDate : pOppDates) {
-        	    if (!pOppDateStr.empty()) {
-        	        pOppDateStr += ",";
-        	    }
-        	    pOppDateStr += "'"+pOppDate["gamedate"]+"'";
-    	    }
-            query += pOppDateStr + ") and pd.pitcherid="+std::to_string(pitcherId)+" and pd.isHitterStarter=1 and pd.event > -9999";
-            query += " order by pd.batpos,pd.hits,ph.gamedate;";
-
-            std::vector<std::map<std::string, std::string>> gameRes = DBWrapper::queryDatabase(db, query);
-
-            if (!gameRes.empty()) {
-            	ss << pitcher << "," << opponent << "\n";
-            }
-
-            for (std::map<std::string, std::string> gr : gameRes) {
-                if (argc > 1) {
-                    ss << ":";
-                }
-                ss << gr["gamedate"] << ",";
-                if (gr["isNightGame"][0] == '1') {
-                    ss << "n";
-                }
-                else if (gr["isNightGame"][0] == '0') {
-                    ss << "d";
-                }
-                ss << "," << gr["batpos"] << "," << gr["hits"] << ","
-                   << gr["inningtype"] << "," << gr["inningnum"] << "," << gr["event"] << "\n";
-            }
-    	}
-
         if (!ss.str().empty()) {
             std::cout << ss.str() << std::endl;
         }
